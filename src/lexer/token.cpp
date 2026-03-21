c0320c7de714a81f3151414bc84e535987808187
Gabriel Ramalho <biel.kinb@gmail.com>
2026-03-21 13:50:34 -0300

    feat: definindo a classe token e seu arquivo header

--- src/lexer/token.cpp
+++ src/lexer/token.cpp
@@ -1,4 +1,5 @@
-
+#include <string>
+#include "token.h"
 
 /*
 ta faltando token ainda eu acho (revisar)
@@ -6,12 +7,19 @@ ta faltando token ainda eu acho (revisar)
 tokens devem ser: palavras reservadas, identificadores, literais (string e int), 
 simbolos (operadores e pontuacao), EOF, error, comentarios e whitespace (se for necessario para o parser, caso contrario pode ser ignorado)
 */
+Lextoken::Lextoken(lextoken_type t, std::string str, int line)
+    : type(t), token_str(str), line_num(line) {}
 
-enum lextoken_type {
-    class_token, if_token, then_token, else_token, fi_token, in_kw_token, inherits_token,
-    isvoid_token, let_token, loop_token, case_token, eof_token, identifier_token, int_token, string_token, 
-    while_token , plus_token, minus_token, esac_token, new_token, of_token, not_token, pool_token,
-    error_token, semicolon_token, lparen_token, rparen_token, lbrace_token, rbrace_token, equal_token,
-    type_token, true_token, false_token, whitespace_token, comment_token, in_token, out_token,
-};
+
+lextoken_type Lextoken::get_type() const {
+    return type;
+}
+
+const std::string& Lextoken::get_token_str() const {
+    return token_str;
+}
+
+int Lextoken::get_line_num() const {
+    return line_num;
+}
 
