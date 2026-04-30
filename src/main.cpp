78bce03edadeb343bd4ce0c7dc1a7c268d60f441
Gabriel Ramalho <biel.kinb@gmail.com>
2026-04-29 23:09:44 -0300

    chore: mudança na main para debug e verificação da etapa de parse

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,15 @@
 #include "lexer.h"
+#include "../headers/parser.h"
 #include <iostream>
 using std::cout;
 
-int main(){
-    Lexer lexer;
-    while(true){
-        auto token = lexer.scan();
-        if(token->get_type() == lextoken_type::eof_token){
-            cout << "Fim do arquivo lido na linha: "<< lexer.get_line_num() << endl;
-            break;
-        }
-        else if(token->get_type() == lextoken_type::error_token){
-            cout << "Token de erro encontrado na linha " << lexer.get_line_num() << "." << endl;
-        }
-
-        /*
-            Como estamos usando polimorfismo, estou usando o dynamic_cast
-            para ver qual o tipo do token e ai printar ou uma string ou um num.
-        */
-        if(auto s = dynamic_cast<StrToken*>(token.get())){
-            if(s->get_type() == lextoken_type::typeID_token){
-                cout << "Token identificador de tipo: " << s->get_token_str() << endl;
-            }
-            else if(s->get_type() == lextoken_type::objectID_token){
-                cout << "Token identificador de objeto: " << s->get_token_str() << endl;
-            }
-            else if(s->get_type() == lextoken_type::string_token){
-                cout << "Token string:  " << s->get_token_str() << endl;
-            }
-            else{
-                cout << "Token palavra reservada:  " << s->get_token_str() << endl;
-            }
-        } else if( auto n = dynamic_cast<NumToken*>(token.get())){
-            cout << "Token tipo numero: " << n->get_token_num() << endl;
-        } else {
-            cout << "Operador ou pontuacao: " << lexer.type_to_string(token->get_type()) << endl;
+int main(int argc, char* argv[]){
+    string filepath = argc > 1 ? argv[1] : "teste.txt";
+    SLRParser* parser = new SLRParser(filepath);
+    parser->parsing();
+    if (parser->get_root()) {
+        cout << "AST construida com sucesso" << endl;
     }
-    }
+    delete parser;
+    return 0;
 }
